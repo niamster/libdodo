5696bce52c85c32d6d3769db5a5ff6a724f7404e
niam <devnull@localhost>
2008-04-09 23:30:55 +0300

    rpcXmlClient, rpcXmlServer started

---
+++ trunk/include/libdodo/rpcXmlClient.h
@@ -0,0 +1,71 @@
+/***************************************************************************
+ *            rpcXmlClient.h
+ *
+ *  Sat Mar 22 00:06:55 2008
+ *  Copyright  2008  Ni@m
+ *  niam.niam@gmail.com
+ ****************************************************************************/
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License version 2.1 as published by
+ *  the Free Software Foundation;
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef _RPCXMLCLIENT_H_
+#define _RPCXMLCLIENT_H_
+
+#include <libdodo/directives.h>
+
+#include <libdodo/types.h>
+#include <libdodo/stringTools.h>
+#include <libdodo/rpcClient.h>
+#include <libdodo/xml.h>
+
+namespace dodo
+{
+	/**
+	 * @class rpcXmlClient defines client-side XML-RPC instrument
+	 */
+	class rpcXmlClient : public rpcClient
+	{
+		public:
+
+			/**
+			 * constructor
+			 */
+			rpcXmlClient();
+
+			/**
+			 * destructor
+			 */
+			virtual ~rpcXmlClient();
+		
+		protected:
+			
+			/**
+			 * process RPC call
+			 * @return RPC method
+			 * @param method defines RPC method representation
+			 */
+			virtual dodoString processRPCCall(const rpcMethod &method);
+			
+			/**
+			 * process RPC call
+			 * @return RPC response represantation
+			 * @param data defines buffer that contains RPC response
+			 */
+			virtual rpcResponse processRPCCallResult(const dodoString &data);
+	};
+};
+
+#endif
---
+++ trunk/include/libdodo/rpcXmlServer.h
@@ -0,0 +1,72 @@
+/***************************************************************************
+ *            rpcXmlServer.h
+ *
+ *  Sat Mar 29 00:06:55 2008
+ *  Copyright  2008  Ni@m
+ *  niam.niam@gmail.com
+ ****************************************************************************/
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License version 2.1 as published by
+ *  the Free Software Foundation;
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef _RPCXMLSERVER_H_
+#define _RPCXMLSERVER_H_
+
+#include <libdodo/directives.h>
+
+#include <libdodo/types.h>
+#include <libdodo/stringTools.h>
+#include <libdodo/rpcServer.h>
+#include <libdodo/xml.h>
+
+namespace dodo
+{
+	
+	/**
+	 * @class rpcXmlServer defines server-side XML-RPC instrument
+	 */
+	class rpcXmlServer : public rpcServer
+	{
+		public:
+
+			/**
+			 * constructor
+			 */
+			rpcXmlServer();
+
+			/**
+			 * destructor
+			 */
+			virtual ~rpcXmlServer();
+		
+		protected:
+			
+			/**
+			 * process RPC call
+			 * @return RPC method represantation
+			 * @param data defines buffer that contains RPC request
+			 */
+			virtual rpcMethod processRPCCall(const dodoString &data);
+			
+			/**
+			 * process RPC call
+			 * @return RPC response
+			 * @param response defines RPC response representation
+			 */
+			virtual dodoString processRPCCallResult(const rpcResponse &response);
+	};
+};
+
+#endif
