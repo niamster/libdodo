c92f2700e70aee9a79cbe58af11c6226f4ad91d9
niam <devnull@localhost>
2008-06-13 02:18:08 +0300

    added test for io::network::ssl::client

---
+++ trunk/tests/ionetworksslclient_test/test.cc
@@ -0,0 +1,46 @@
+
+#include <libdodo/baseEx.h>
+#include <libdodo/ioNetworkSslClient.h>
+#include <libdodo/types.h>
+
+#include <iostream>
+
+using namespace dodo;
+using namespace io::network::ssl;
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	try
+	{	
+		dodoString host = "sourceforge.net";
+
+		cout << tools::network::getHostPrimaryIp(host) << endl;
+
+		client st(io::network::OPTIONS_PROTO_FAMILY_IPV4,io::network::OPTIONS_TRANSFER_TYPE_STREAM);
+		exchange exch;
+		dodoString str;
+		
+		st.connect(tools::network::getHostPrimaryIp(host),443,exch);
+
+		exch.writeStreamString("GET / HTTP/1.0\r\n");
+		exch.writeStreamString("Host: "+ host +"\r\n");
+		exch.writeStreamString("\r\n");
+
+		while (true)
+		{
+			exch.readStreamString(str);
+			if (str.size() == 0)
+				break;
+			cout << str;
+		}
+	}
+	catch(baseEx ex)
+	{
+		cout << (string)ex << "\t" << ex.line << "\t" << ex.file << endl;
+		cout.flush();
+	}
+	
+	return 0;
+}
