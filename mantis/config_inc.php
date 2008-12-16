72dcbf017084f8f9a069e363555796c4ffde0fe1
niam <devnull@localhost>
2008-12-16 17:08:04 +0200

    added mantis configuration for hg integration to resolve issue #29

---
+++ mantis/config_inc.php
@@ -0,0 +1,7 @@
+<?php
+	$g_source_control_account            = 'hg';
+	$g_source_control_regexp             = '/(?:bug|issue)\s*[#]{0,1}(\d+)/im';
+	$g_source_control_set_status_to      = RESOLVED;
+	$g_source_control_set_resolution_to  = FIXED;
+	$g_source_control_fixed_regexp       = '/(fixed|resolv)(?:ed|es|e).+(?:bug|issue)?\s*[#]{0,1}(\d+)/im';
+?>
