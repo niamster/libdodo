a6e1e12d1ad81ee06bf7e14d98bb570b67a59553
niam <devnull@localhost>
2009-05-27 08:13:43 +0300

    sample rpc json client/server programs

--- trunk/tests/rpcjsonhttpclient_test/test.cc
+++ trunk/tests/rpcjsonhttpclient_test/test.cc
@@ -5,25 +5,109 @@
  */
 
 
-#include <libdodo/exceptionBasic.h>
-#include <libdodo/rpcJsonHttpClient.h>
-#include <libdodo/types.h>
+#include <libdodo/dodo.h>
 
 #include <iostream>
+#include <string.h>
 
 using namespace dodo;
 using namespace rpc;
 
 using namespace std;
 
+class httpIO : public io::channel, public io::network::http
+{
+  public:
+
+	httpIO() : io::channel(io::CHANNEL_PROTECTION_NONE),
+			   response(NULL)
+	{
+	}
+
+  protected:
+
+	virtual void _read(char * const data)
+	{
+		_readStream(data);
+	}
+
+	virtual unsigned long _readStream(char * const data)
+	{
+		unsigned long size = 0;
+
+		if (response != NULL)
+		{
+			size = response->data.size();
+			if (size != 0)
+			{
+				if (size > inSize)
+					size = inSize;
+
+				memcpy(data, response->data.data(), size);
+				if (size < inSize)
+					memset(data+size, 0x0, inSize - size);
+			}
+
+			delete response;
+			response = NULL;
+		}
+
+		return size;
+	}
+
+	virtual void _write(const char * const data)
+	{
+		if (response != NULL)
+		{
+			delete response;
+			response = NULL;
+		}
+
+		dodoString post_data(data, outSize);
+
+		response = new io::network::__httpResponse__(POST(post_data, "application/json"));
+	}
+
+	virtual void _writeStream(const char * const data)
+	{
+		if (response != NULL)
+		{
+			delete response;
+			response = NULL;
+		}
+
+		unsigned int dataLen = strlen(data);
+
+		dodoString post_data(data, dataLen>outSize?outSize:dataLen);
+
+		response = new io::network::__httpResponse__(POST(post_data, "application/json"));
+	}
+
+	int getOutDescriptor() const
+	{
+		return -1;
+	}
+
+	int getInDescriptor() const
+	{
+		return -1;
+	}
+
+	void flush()
+	{
+	}
+
+	io::network::__httpResponse__ *response;
+};
 
 int main(int argc, char **argv)
 {
 	try
 	{
-		rpc::json::http::client client;
+		httpIO http;
+		json::client client(http);
 
-		client.setUrl("http://localhost/libdodo/rpcjsoncgiserver_test/test.cgi");
+		http.setUrl("http://localhost/libdodo/rpcjsoncgiserver_test/test.cgi");
 
 		method method;
 		value argument;
@@ -50,7 +134,7 @@ int main(int argc, char **argv)
 		cout << "Response ID: " << client.getResponseId() << endl;
 		cout << "First value: " << resp.getValue().getString() << endl;
 		cout << "Second value: " << resp.getValue(1).getString() << endl;
-		
+
 		resp = client.call(method);
 
 		cout << "Amount of values: " << resp.getValues().size() << endl;
